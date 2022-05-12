b5565992b88534215dc822ec175cb7ab617ba118
Jheniffer-fernandes <jheniffer.fernandesm@gmail.com>
2022-05-11 21:50:46 -0300

    subindo aula 07 JS

---
+++ JS/Aula07_Estruturas Condicionais/exercício.js
@@ -0,0 +1,33 @@
+const colaboradores = Array = [
+    {
+    firstName: 'Aaron',
+    lastName: 'Reid',
+    salario: 1500,
+    vinculo: 'clt'
+},
+{
+    firstName: 'Agatha',
+    lastName: 'Navarro',
+    salario: 2500,
+    vinculo: 'pj'
+},
+{
+    firstName: 'James',
+    lastName: 'Stones',
+    salario: 3500,
+    vinculo: 'clt'
+},
+{
+    firstName: 'Kalila',
+    lastName: 'Sparks',
+    salario: 5000,
+    vinculo: 'clt'
+},
+{
+    firstName: 'Lysandre',
+    lastName: 'Jenkins',
+    salario: 4500,
+    vinculo: 'pj'
+},
+];
+
