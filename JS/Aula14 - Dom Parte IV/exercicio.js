43fb03f9b9c79f3566ea8ef36c98962a1fb9c2ab
Jheniffer-fernandes <jheniffer.fernandesm@gmail.com>
2022-05-26 21:30:44 -0300

    att exercicio

--- JS/Aula14 - Dom Parte IV/exercicio.js
+++ JS/Aula14 - Dom Parte IV/exercicio.js
@@ -1,104 +1,112 @@
-
-
-const myForm = document.querySelector('#form');
-const nameInput = document.querySelector('#oame');
-const emailInput = document.querySelector('#dNascimento');
-const nascimento = document.querySelector('#email');
-const msg = document.querySelector('.msg');
-const msg_email = document.querySelector('.msg_email');
+const nome = document.querySelector('#nome');
+const bDay = document.querySelector('#dNascimento');
+const email = document.querySelector('#email');
 const userList = document.querySelector('#users');
-
-Form.addEventListener('Incluir', onSubmit);
-Form.addEventListener('Excluir', onSubmit); //excluir
-
+const btnIncluir = document.querySelector('#incluir');
+const pulseira = document.querySelector('#pulseira');
 
 
-function onSubmit(s) {
-    s.preventDefault();
-    
-    // console.log(nameInput.value);
-    
-    if (nameInput.value === '' || emailInput.value === '') {
-        // alert('Por favor, preencha os dados.');
-        msg.classList.add('error');
-        msg.innerHTML = 'Por favor, preencha os dados.';
-        setTimeout(() => msg.remove(), 3000);
-    } else {
-        
-        // console.log('sucess');
-        
-        const li = document.createElement('li');
-        li.appendChild(
-            document.createTextNode(
-                `${nameInput.value} : ${emailInput.value} : ${nascimento.value}`
-            )
-        );
-        userList.appendChild(li);
-        
-        // Limpa o formulário
-        nameInput.value = '';
-        emailInput.value = '';
-        horario.getElementsByTagName('option')[0].selected = 'selected';
-        nameInput.focus(); //Coloca o foco no elmento
-    }
-}
+btnIncluir.addEventListener('click', Incluir);
 
-function onExclude(e) {
+function Incluir(e) {
     e.preventDefault();
+
+    let li = document.createElement('li');
+    userList.appendChild(li);
+    li.innerHTML = `${nome.value} | ${bDay.value} | ${email.value}`
+};
+
+
+
+
+
+
+// function onSubmit(s) {
+//     s.preventDefault();
     
     // console.log(nameInput.value);
     
-    if (nameInput.value === '' || emailInput.value === '') {
-        // alert('Por favor, preencha os dados.');
-        msg.classList.add('error');
-        msg.innerHTML = 'Por favor, preencha os dados.';
-        setTimeout(() => msg.remove(), 3000);
-    } else {
+    // if (nameInput.value === '' || emailInput.value === '') {
+    //     // alert('Por favor, preencha os dados.');
+    //     msg.classList.add('error');
+    //     msg.innerHTML = 'Por favor, preencha os dados.';
+    //     setTimeout(() => msg.remove(), 3000);
+    // } else {
         
         // console.log('sucess');
         
-        const li = document.createElement('li');
-        li.appendChild(
-            document.createTextNode(
-                `${nameInput.value} : ${emailInput.value} : ${horario.value}`
-            )
-        );
-        userList.appendChild(li);
+//         const li = document.createElement('li');
+//         li.appendChild(
+//             document.createTextNode(
+//                 `${nameInput.value} : ${emailInput.value} : ${nascimento.value}`
+//             )
+//         );
+//         userList.appendChild(li);
         
-        // Limpa o formulário
-        nameInput.value = '';
-        emailInput.value = '';
-        horario.getElementsByTagName('option')[0].selected = 'selected';
-        nameInput.focus(); //Coloca o foco no elmento
-    }
-}
+//         // Limpa o formulário
+//         nameInput.value = '';
+//         emailInput.value = '';
+//         horario.getElementsByTagName('option')[0].selected = 'selected';
+//         nameInput.focus(); //Coloca o foco no elmento
+//     }
+// }
 
-// Validando e-mail
-
-emailInput.addEventListener('change', (e) => {
-    let padrao = new RegExp(/.*@.*\..*/i);
-    if (!padrao.test(emailInput.value)) {
+// function onExclude(e) {
+//     e.preventDefault();
+    
+//     // console.log(nameInput.value);
+    
+//     if (nameInput.value === '' || emailInput.value === '') {
+//         // alert('Por favor, preencha os dados.');
+//         msg.classList.add('error');
+//         msg.innerHTML = 'Por favor, preencha os dados.';
+//         setTimeout(() => msg.remove(), 3000);
+//     } else {
         
-        // alert('Por favor, insira um e-mail válido.');
-        msg_email.classList.add('error');
-        setTimeout(() => msg.email(), 3000);
-        msg_email.innerHTML = 'Por favor, insira um e-mail válido.';
-        setTimeout(() => msg.remove(), 3000);
-
-    }
-});
-
-// Base validar idade
-
-emailInput.addEventListener('change', (e) => {
-    let padrao = new RegExp(/.*@.*\..*/i);
-    if (!padrao.test(emailInput.value)) {
+//         // console.log('sucess');
         
-        // alert('Por favor, insira um e-mail válido.');
-        msg_email.classList.add('error');
-        setTimeout(() => msg.email(), 3000);
-        msg_email.innerHTML = 'Por favor, insira um e-mail válido.';
-        setTimeout(() => msg.remove(), 3000);
+//         const li = document.createElement('li');
+//         li.appendChild(
+//             document.createTextNode(
+//                 `${nameInput.value} : ${emailInput.value} : ${horario.value}`
+//             )
+//         );
+//         userList.appendChild(li);
+        
+//         // Limpa o formulário
+//         nameInput.value = '';
+//         emailInput.value = '';
+//         horario.getElementsByTagName('option')[0].selected = 'selected';
+//         nameInput.focus(); //Coloca o foco no elmento
+//     }
+// }
 
-    }
-});
+// // Validando e-mail
+
+// emailInput.addEventListener('change', (e) => {
+//     let padrao = new RegExp(/.*@.*\..*/i);
+//     if (!padrao.test(emailInput.value)) {
+        
+//         // alert('Por favor, insira um e-mail válido.');
+//         msg_email.classList.add('error');
+//         setTimeout(() => msg.email(), 3000);
+//         msg_email.innerHTML = 'Por favor, insira um e-mail válido.';
+//         setTimeout(() => msg.remove(), 3000);
+
+//     }
+// });
+
+// // Base validar idade
+
+// emailInput.addEventListener('change', (e) => {
+//     let padrao = new RegExp(/.*@.*\..*/i);
+//     if (!padrao.test(emailInput.value)) {
+        
+//         // alert('Por favor, insira um e-mail válido.');
+//         msg_email.classList.add('error');
+//         setTimeout(() => msg.email(), 3000);
+//         msg_email.innerHTML = 'Por favor, insira um e-mail válido.';
+//         setTimeout(() => msg.remove(), 3000);
+
+//     }
+// });
