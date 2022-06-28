929e15ae0610138be8d0817a0a21f760354be985
Jheniffer-fernandes <jheniffer.fernandesm@gmail.com>
2022-06-28 20:48:29 -0300

    'subindo aula java'

---
+++ Java/08_Vetores/Exercícios/Exercícios/src/MenorMaiorMediaValores.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class MenorMaiorMediaValores {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int menor;
+        int maior;
+        int media = 0;
+        int[] numeros = new int[10];
+
+        // comprimento do vetor
+
+        int tamanho = numeros.length;
+
+        for (int i = 0; i < tamanho; i++) {
+            System.out.println("Digite um número:");
+            numeros[i] = input.nextInt();
+        }
+        menor = numeros[0];
+        maior = numeros[0];
+
+        // encontrar o maior número
+
+        for (int i = 0; i < tamanho; i++) {
+            if (numeros[i] > maior) {
+                maior = numeros[i];
+            }
+        }
+        // encontrar o menor número
+
+        for (int i = 0; i < tamanho; i++) {
+            if (numeros[i] < menor) {
+                menor = numeros[i];
+            }
+        }
+        for (int i = 0; i < tamanho; i++) {
+            if (numeros[i] < menor) {
+                menor = numeros[i];
+            }
+        }
+        // encontrar a media dos números
+        for (int counter = 0; counter < numeros.length; counter++)
+            media += numeros[counter] / 10;
+
+        System.out.println("O maior número é: "+maior);
+        System.out.println("O menor número é: "+menor);
+        System.out.println("A media dos números é: "+media);
+        input.close();
+    }
+
+}
---
+++ Java/08_Vetores/Exercícios/Exercícios/src/Nomes.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Nomes {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        // declaração de vetor
+        String[] nome = new String[5];
+
+
+        // entrada de dados
+        for(String nome = 0; nome < nome.length; i++){
+            System.out.println("Digite um número");
+            vetor[i] = ent.nextInt();
+        }
+
+        // saída de dados -- inversa
+        for(int i = vetor.length-1; i >= 0; i--){
+            System.out.println(vetor[i]);
+        }
+
+    }
+}
